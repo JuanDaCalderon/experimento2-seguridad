fed617e0a0e0f091a3809b2121329197d63cc859
IngSneyderAmado <s.amadov@uniandes.edu.co>
2023-09-26 20:44:51 -0500

    se crea enpoint de acceso

---
+++ acceso/app.py
@@ -0,0 +1,19 @@
+from autorizador import create_app
+from .modelos import db
+from flask_restful import Api
+from flask_cors import CORS
+from .vistas import VistaAcceso
+from flask_jwt_extended import JWTManager
+
+app = create_app()
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+cors = CORS(app)
+
+api = Api(app)
+api.add_resource(VistaAcceso, '/acceso')
+jwt = JWTManager(app)
---
+++ acceso/modelos/modelos.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+db = SQLAlchemy()
+class Menu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50))
+    url = db.Column(db.String(50))
+    progrma = db.Column(db.String())
+    
+class MenuSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Menu
+        load_instance = True
+    id = fields.String()
+    nombre = fields.String()
+    url = fields.String()
+    programa = fields.String()
+    
