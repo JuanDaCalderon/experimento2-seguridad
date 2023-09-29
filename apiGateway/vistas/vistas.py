3d2298c71d68cf6d98572997ea1514750b84f4fd
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2023-09-28 19:19:31 -0500

    Se finaliza API Gateway

--- apiGateway/vistas/vistas.py
+++ apiGateway/vistas/vistas.py
@@ -1,5 +1,5 @@
 from flask import request
-from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt, get_csrf_token
 import requests
 from flask_restful import Resource
 
@@ -13,33 +13,25 @@ class VistaLogin(Resource):
 
 
 class VistaDarAcceso(Resource):
-    """@jwt_required()
+    @jwt_required()
     def post(self):
         idTokenUsuario = get_jwt_identity()
         print("Aquí es token id", idTokenUsuario)
-        tokenUsuario = get_jwt()
-        print("Aquí es usuario token", tokenUsuario)"""
+        tokenOriginalCompleto = request.headers.get('Authorization')
+        tokenOriginal = tokenOriginalCompleto.split(' ')[1] if tokenOriginalCompleto else None
+        print(tokenOriginal)
 
-    @jwt_required()
-    def post(self):
-        data = request.get_json()
-        autorizador = "http://localhost:5002/acceso"
-        headers =  {"Content-Type":"application/json", "Authorization": f"Bearer {data}"}
-        respuesta = requests.get(autorizador, headers=headers)
-        return respuesta.json()
-    
-        """print(tokenUsuario)
-        autorizador = "http://localhost:5000/acceso"
-        respuestaToken = requests.post(autorizador, json={"usuario": idTokenUsuario})
+        autorizador = "http://localhost:5001/verificar"
+        data = {
+            "id": idTokenUsuario,
+            "token": tokenOriginal
+        }
+        respuesta = requests.post(autorizador, json=data)
 
-        if respuestaToken.status_code == 200:
-            acceso = "/acceso"
-            headers =  {"Content-Type":"application/json", "Authorization": f"Bearer {idTokenUsuario}"}
-            respuestaAcceso = requests.get(acceso, headers=headers)
-
-            return respuestaAcceso
+        if respuesta.status_code == 200:
+            acceso = "http://localhost:5002/acceso"
+            headers =  {"Content-Type":"application/json", "Authorization": f"Bearer {tokenOriginal}"}
+            respuestaAcceso = requests.get(acceso, json='', headers=headers)
+            return respuestaAcceso.json()
         
-        return {"tokenid": idTokenUsuario, "tokenUser": tokenUsuario}
-            
-
-        #Pregunta al autorizador si el token es valido y si es valido consume url acceso"""
+        return {"Mensaje": "Error"}
